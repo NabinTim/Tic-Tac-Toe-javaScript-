3bd76fc8c736874047b6617789647fa68c43a5ad
NabinTim <timalsinanabin41@gmail.com>
2024-05-03 01:08:50 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const cells = document.querySelectorAll('.cell');
+const blueButton = document.getElementById('blueButton');
+const redButton = document.getElementById('redButton');
+const resetButton = document.getElementById('resetButton');
+const title = document.getElementById('title');
+
+let teamSign = '〇'; 
+
+
+function resetGame() {
+
+    title.textContent = 'You are on a Red Team';
+    
+
+    teamSign = '〇';
+
+
+    cells.forEach(cell => {
+        cell.textContent = '';
+    });
+}
+
+blueButton.onclick = function() {
+
+    title.textContent = 'You are on a Blue Team';
+    
+
+    teamSign = '〇';
+};
+
+
+function cellClickHandler(event) {
+    const cell = event.target;
+    if (!cell.textContent) { 
+        cell.textContent = teamSign; 
+    }
+}
+
+
+cells.forEach(cell => {
+    cell.onclick = cellClickHandler;
+});
+
+redButton.onclick = function() {
+
+    teamSign = '✗'; 
+    
+
+    title.textContent = 'You are on a Red Team';
+};
+
+
+resetButton.onclick = resetGame;
